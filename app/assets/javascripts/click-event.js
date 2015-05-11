d437d4759b613f78c0627f2d708bb97b15fedf37
Dan Berger <dsberger@gmail.com>
2015-05-11 15:04:43 -0700

    submit button default prevented

--- app/assets/javascripts/click-event.js
+++ app/assets/javascripts/click-event.js
@@ -1,12 +1,10 @@
-$( document ).ready(function() {
+$( document ).ready(function(){
   $( '#background' ).click(function(e){
     var x = e.pageX;
     var y = e.pageY;
     addFrame(x, y);
     addDropdown(x, y);
   });
-
-
 
   var addFrame = function(x, y){
     var $frame = $( "<div>", {class: 'frame' } );
@@ -24,14 +22,14 @@ $( document ).ready(function() {
   };
 
   var addDropdown = function(x, y){
-    var $dropdown = $( "<div>", {class: 'dropdown' } );
+    var $dropdown = $( "<div>", { class: 'dropdown' } );
     $dropdown.css( "margin-top", y );
     $dropdown.css( "margin-left", x );
     $dropdown.append( "<form id='character-selection'><select name='characters'><option value='waldo'>Waldo</option><option value='wenda'>Wenda</option><option value='odlaw'>Odlaw</option><option value='wizard'>Wizard Whitebeard</option><option value='woof'>Woof</option></select><input type='submit' value='Submit' class='submit'></form>");
+    $( '#container' ).append( $dropdown );
     $( '.submit' ).click(function(event){
       event.preventDefault();
-      console.log(this);
+      console.log( "hello?" );
     });
-    $( '#container' ).append( $dropdown );
   };
 });
