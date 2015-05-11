4d90d4cfa679af3f1cd3562396f208e7f365812e
Dan Berger <dsberger@gmail.com>
2015-05-11 11:43:06 -0700

    frame added to picture at correct Y level:

--- app/assets/javascripts/click-event.js
+++ app/assets/javascripts/click-event.js
@@ -3,7 +3,8 @@ $( document ).ready(function() {
     var x = e.pageX;
     var y = e.pageY;
     console.log("X: " + x + ", Y: " + y);
-    $frame = $( "<div>", { class: 'frame' } );
-    $( '#container' ).append( $frame )
+    $frame = $( "<div>", {class: 'frame', } );
+    $frame.css( "margin-top", y );
+    $( '#container' ).append( $frame );
   });
 });
