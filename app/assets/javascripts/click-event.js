1ce45ae2505b20034868cfe226f03a99aad8c849
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-05-11 16:43:12 -0600

    adds character name to frame upon tag

--- app/assets/javascripts/click-event.js
+++ app/assets/javascripts/click-event.js
@@ -25,15 +25,17 @@ var clickEvent = {
     $closeFrame.click(function() {
       this.parentElement.remove();
     });
+    $( '.submit' ).click(function(event){
+      event.preventDefault();
+      var name = $("select").val();
+      var $name = $( "<div>", { class: 'name', text: name } );
+      $frame.append( $name );
+    });
   },
 
   addDropdown: function(frame){
     var $dropdown = $( "<div>", { class: 'dropdown' } );
-    $dropdown.append( "<form id='character-selection'><select name='characters'><option value='waldo'>Waldo</option><option value='wenda'>Wenda</option><option value='odlaw'>Odlaw</option><option value='wizard'>Wizard Whitebeard</option><option value='woof'>Woof</option></select><input type='submit' value='Submit' class='submit'></form>");
+    $dropdown.append( "<form id='character-selection'><select name='characters'><option value='Waldo'>Waldo</option><option value='Wenda'>Wenda</option><option value='Odlaw'>Odlaw</option><option value='Wizard'>Wizard Whitebeard</option><option value='Woof'>Woof</option></select><input type='submit' value='Submit' class='submit'></form>");
     frame.append($dropdown);
-    $( '.submit' ).click(function(event){
-      event.preventDefault();
-      console.log( "hello?" );
-    });
   }
 }
